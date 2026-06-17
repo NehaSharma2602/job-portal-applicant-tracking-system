5544d86bb8a79c4e39a343a24819c209fecb143b
Neha Sharma <neha2sharma15@gmail.com>
2026-06-17 19:36:33 +0530

    Initial project structure and package organization

---
+++ src/main/java/com/Neha/job_portal_applicant_tracking_system/JobPortalApplicantTrackingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.Neha.job_portal_applicant_tracking_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobPortalApplicantTrackingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobPortalApplicantTrackingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Neha/job_portal_applicant_tracking_system/JobPortalApplicantTrackingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Neha.job_portal_applicant_tracking_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobPortalApplicantTrackingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
