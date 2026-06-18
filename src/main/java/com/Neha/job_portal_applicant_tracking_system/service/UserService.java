bb4f61082bc0cd7ccab7471150b04c2f17f86335
Neha Sharma <neha2sharma15@gmail.com>
2026-06-18 19:32:42 +0530

    Created UserServiceInterface

---
+++ src/main/java/com/Neha/job_portal_applicant_tracking_system/service/UserService.java
@@ -0,0 +1,25 @@
+package com.Neha.job_portal_applicant_tracking_system.service;
+
+import java.util.List;
+
+import com.Neha.job_portal_applicant_tracking_system.dto.UserRequestDTO;
+import com.Neha.job_portal_applicant_tracking_system.dto.UserResponseDTO;
+
+public interface UserService {
+
+	UserResponseDTO registerUser(UserRequestDTO userRequestDTO);
+	
+	UserResponseDTO getUserById(Long id);
+	
+	UserResponseDTO getUserByEmail(String email);
+	
+	List<UserResponseDTO> getAllUser();
+	
+	UserResponseDTO updateuser(Long id, UserRequestDTO userRequestDTO);
+	
+	void deactivateUser(Long id);
+	
+	void deleteUser(Long id);
+	
+	boolean emailExists(String email);
+}
