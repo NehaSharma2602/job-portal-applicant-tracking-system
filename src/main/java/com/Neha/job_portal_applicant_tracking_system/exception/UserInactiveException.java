e62dda88a00404cb09a212469d108315ac796019
Neha Sharma <neha2sharma15@gmail.com>
2026-06-19 17:28:45 +0530

    Created UserInactiveException

---
+++ src/main/java/com/Neha/job_portal_applicant_tracking_system/exception/UserInactiveException.java
@@ -0,0 +1,10 @@
+package com.Neha.job_portal_applicant_tracking_system.exception;
+
+public class UserInactiveException extends RuntimeException {
+	
+	public UserInactiveException(String message) {
+		// TODO Auto-generated constructor stub
+		super(message);
+	}
+
+}
