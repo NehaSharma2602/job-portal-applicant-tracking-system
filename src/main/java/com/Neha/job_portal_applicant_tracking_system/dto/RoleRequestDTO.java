8490dcce56ce8bf58e13d1c9f8f193a31a72dc49
Neha Sharma <neha2sharma15@gmail.com>
2026-06-19 17:12:59 +0530

    Created RoleRequestDTO

---
+++ src/main/java/com/Neha/job_portal_applicant_tracking_system/dto/RoleRequestDTO.java
@@ -0,0 +1,15 @@
+package com.Neha.job_portal_applicant_tracking_system.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RoleRequestDTO {
+
+	private String role;
+}
