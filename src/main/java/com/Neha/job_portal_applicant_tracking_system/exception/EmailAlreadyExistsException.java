dc7f163d8e9750fb83686cb51b2ddf16b570ee27
Neha Sharma <neha2sharma15@gmail.com>
2026-06-19 17:26:53 +0530

    Created EmailAlreadyExistsException

---
+++ src/main/java/com/Neha/job_portal_applicant_tracking_system/exception/EmailAlreadyExistsException.java
@@ -0,0 +1,10 @@
+package com.Neha.job_portal_applicant_tracking_system.exception;
+
+public class EmailAlreadyExistsException extends RuntimeException{
+	
+	public EmailAlreadyExistsException(String message) {
+		// TODO Auto-generated constructor stub
+		super(message);
+	}
+
+}
