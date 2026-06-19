871f74ca96214f25fb978b3fbd91dd26df637e1e
Neha Sharma <neha2sharma15@gmail.com>
2026-06-19 17:23:42 +0530

    Created RoleServiceInterface

---
+++ src/main/java/com/Neha/job_portal_applicant_tracking_system/service/RoleService.java
@@ -0,0 +1,23 @@
+package com.Neha.job_portal_applicant_tracking_system.service;
+
+import java.util.List;
+
+import com.Neha.job_portal_applicant_tracking_system.dto.RoleRequestDTO;
+import com.Neha.job_portal_applicant_tracking_system.dto.RoleResponseDTO;
+
+public interface RoleService {
+
+	//create
+	RoleResponseDTO createRole(RoleRequestDTO roleRequestDTO);
+	
+	//get role by id
+	RoleResponseDTO getRoleById(Long id);
+	
+	// get all role
+	List<RoleResponseDTO> getAllRole();
+	
+	//get role by name
+	RoleResponseDTO getRoleByName(String role);
+	
+	void deleteRole(Long id);
+}
