0eecdad6dc872c318a1b8d39b0065bbd54c2038e
Neha Sharma <neha2sharma15@gmail.com>
2026-06-24 19:26:38 +0530

    Created RoleAlreadyExitsException

---
+++ src/main/java/com/Neha/job_portal_applicant_tracking_system/exception/RoleAlreadyExistsException.java
@@ -0,0 +1,11 @@
+package com.Neha.job_portal_applicant_tracking_system.exception;
+
+public class RoleAlreadyExistsException extends Exception{
+
+	public RoleAlreadyExistsException(String message) {
+		super(message);
+	}
+	
+	
+
+}
