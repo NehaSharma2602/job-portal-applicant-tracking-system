9b6a80d39f1610074d670fc61070b932c2849c5e
Neha Sharma <neha2sharma15@gmail.com>
2026-06-25 19:51:45 +0530

    created enum jobtype class

---
+++ src/main/java/com/Neha/job_portal_applicant_tracking_system/entity/JobType.java
@@ -0,0 +1,13 @@
+package com.Neha.job_portal_applicant_tracking_system.entity;
+
+import jakarta.persistence.Entity;
+
+public enum JobType {
+
+	FULL_TIME,
+    PART_TIME,
+    CONTRACT,
+    INTERNSHIP,
+    FREELANCE,
+    REMOTE
+}
