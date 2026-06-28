d65f5cc58cc54a5f9fb8a378c37fc63b7393733b
Neha Sharma <neha2sharma15@gmail.com>
2026-06-28 12:14:06 +0530

    created companynotfoundexception

---
+++ src/main/java/com/Neha/job_portal_applicant_tracking_system/exception/CompanyNotFoundException.java
@@ -0,0 +1,10 @@
+package com.Neha.job_portal_applicant_tracking_system.exception;
+
+public class CompanyNotFoundException extends RuntimeException {
+
+	public CompanyNotFoundException(String message) {
+		super(message);
+	}
+
+	
+}
