2de86863316b86f871b06207f370cb6e1ff7e127
Neha Sharma <neha2sharma15@gmail.com>
2026-06-28 12:22:36 +0530

    Created JobNotOpenException

---
+++ src/main/java/com/Neha/job_portal_applicant_tracking_system/exception/JobNotOpenException.java
@@ -0,0 +1,10 @@
+package com.Neha.job_portal_applicant_tracking_system.exception;
+
+public class JobNotOpenException extends RuntimeException {
+
+	public JobNotOpenException(String message) {
+		super(message);
+	}
+
+	
+}
