729de2c437ef99b8768de645c9f54e2e1e71cb7d
Neha Sharma <neha2sharma15@gmail.com>
2026-06-28 12:17:21 +0530

    Created CompanyInactiveException

--- src/main/java/com/Neha/job_portal_applicant_tracking_system/exception/CompanyAlreadyExistsException.java
+++ src/main/java/com/Neha/job_portal_applicant_tracking_system/exception/CompanyAlreadyExistsException.java
@@ -1,5 +1,10 @@
 package com.Neha.job_portal_applicant_tracking_system.exception;
 
-public class CompanyAlreadyExistsException {
+public class CompanyAlreadyExistsException extends RuntimeException {
 
+	public CompanyAlreadyExistsException(String message) {
+		super(message);
+	}
+
+	
 }
---
+++ src/main/java/com/Neha/job_portal_applicant_tracking_system/exception/CompanyInactiveException.java
@@ -0,0 +1,10 @@
+package com.Neha.job_portal_applicant_tracking_system.exception;
+
+public class CompanyInactiveException extends RuntimeException{
+
+	public CompanyInactiveException(String message) {
+		super(message);
+	}
+
+	
+}
