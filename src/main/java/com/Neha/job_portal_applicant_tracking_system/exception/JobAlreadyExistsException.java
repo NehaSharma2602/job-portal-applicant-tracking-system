eda639bd5bfeedad4520c336d3081ee2653e102b
Neha Sharma <neha2sharma15@gmail.com>
2026-06-28 12:20:44 +0530

    JobAlreadyExistsException

---
+++ src/main/java/com/Neha/job_portal_applicant_tracking_system/exception/JobAlreadyExistsException.java
@@ -0,0 +1,10 @@
+package com.Neha.job_portal_applicant_tracking_system.exception;
+
+public class JobAlreadyExistsException extends RuntimeException {
+
+	public JobAlreadyExistsException(String message) {
+		super(message);
+	}
+
+	
+}
