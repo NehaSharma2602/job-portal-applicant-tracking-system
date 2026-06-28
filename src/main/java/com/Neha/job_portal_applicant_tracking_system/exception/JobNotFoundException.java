ff1c7f466a41f58d0fdca89951c87f54c5d39db2
Neha Sharma <neha2sharma15@gmail.com>
2026-06-28 12:19:00 +0530

    Created JobNotFoundException

---
+++ src/main/java/com/Neha/job_portal_applicant_tracking_system/exception/JobNotFoundException.java
@@ -0,0 +1,10 @@
+package com.Neha.job_portal_applicant_tracking_system.exception;
+
+public class JobNotFoundException extends RuntimeException{
+
+	public JobNotFoundException(String message) {
+		super(message);
+	}
+
+	
+}
