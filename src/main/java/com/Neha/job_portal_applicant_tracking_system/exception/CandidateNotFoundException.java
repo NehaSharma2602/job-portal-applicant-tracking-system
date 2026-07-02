ce8b96abf5e011bb5f125a11e70e3332f623a592
Neha Sharma <neha2sharma15@gmail.com>
2026-07-02 15:44:00 +0530

    CandidateNotFoundException

---
+++ src/main/java/com/Neha/job_portal_applicant_tracking_system/exception/CandidateNotFoundException.java
@@ -0,0 +1,7 @@
+package com.Neha.job_portal_applicant_tracking_system.exception;
+
+public class CandidateNotFoundException extends RuntimeException {
+    public CandidateNotFoundException(String message) {
+        super(message);
+    }
+}
