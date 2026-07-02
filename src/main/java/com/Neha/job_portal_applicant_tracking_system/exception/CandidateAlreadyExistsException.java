4b3fde125e618f75031bbfa3b7af6eda8f971da5
Neha Sharma <neha2sharma15@gmail.com>
2026-07-02 15:43:07 +0530

    Created CandidateAlreadyExistsException

---
+++ src/main/java/com/Neha/job_portal_applicant_tracking_system/exception/CandidateAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.Neha.job_portal_applicant_tracking_system.exception;
+
+public class CandidateAlreadyExistsException extends RuntimeException {
+    public CandidateAlreadyExistsException(String message) {
+        super(message);
+    }
+}
