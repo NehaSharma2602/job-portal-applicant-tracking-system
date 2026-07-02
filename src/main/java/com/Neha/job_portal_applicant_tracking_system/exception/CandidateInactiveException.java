6fc5958b501747ecf3a0c74bcff4b906b0b55d74
Neha Sharma <neha2sharma15@gmail.com>
2026-07-02 15:43:36 +0530

    CandidateInactiveException

---
+++ src/main/java/com/Neha/job_portal_applicant_tracking_system/exception/CandidateInactiveException.java
@@ -0,0 +1,7 @@
+package com.Neha.job_portal_applicant_tracking_system.exception;
+
+public class CandidateInactiveException extends RuntimeException {
+    public CandidateInactiveException(String message) {
+        super(message);
+    }
+}
