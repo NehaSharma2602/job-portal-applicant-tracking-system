6603e51d07072457ed7aee54aa8222d6cc93e2f6
Neha Sharma <neha2sharma15@gmail.com>
2026-07-02 15:27:21 +0530

    Updated CandidateResponseDTO

--- src/main/java/com/Neha/job_portal_applicant_tracking_system/dto/CandidateRequestDTO.java
+++ src/main/java/com/Neha/job_portal_applicant_tracking_system/dto/CandidateRequestDTO.java
@@ -2,23 +2,9 @@ package com.Neha.job_portal_applicant_tracking_system.dto;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
 
 import com.Neha.job_portal_applicant_tracking_system.entity.Gender;
 import com.Neha.job_portal_applicant_tracking_system.entity.User;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Min;
