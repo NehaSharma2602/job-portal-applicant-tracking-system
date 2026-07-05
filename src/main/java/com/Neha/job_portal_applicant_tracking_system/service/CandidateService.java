d6c4955e7df0b032188a015c8387d6d8f8c4ba85
Neha Sharma <neha2sharma15@gmail.com>
2026-07-05 12:31:00 +0530

    Created CandidateService Interface

---
+++ src/main/java/com/Neha/job_portal_applicant_tracking_system/service/CandidateService.java
@@ -0,0 +1,40 @@
+package com.Neha.job_portal_applicant_tracking_system.service;
+
+import java.util.List;
+
+import com.Neha.job_portal_applicant_tracking_system.dto.CandidateRequestDTO;
+import com.Neha.job_portal_applicant_tracking_system.dto.CandidateResponseDTO;
+import com.Neha.job_portal_applicant_tracking_system.entity.Gender;
+
+public interface CandidateService {
+
+	CandidateResponseDTO createCandidate(CandidateRequestDTO candidateRequestDTO);
+	
+	CandidateResponseDTO getCandidateById(Long id);
+	
+	CandidateResponseDTO getCandidateByEmail(String email);
+	
+	CandidateResponseDTO getCandidateByUserId(Long userId);
+	
+	List<CandidateResponseDTO> getAllCandidate();
+	
+	List<CandidateResponseDTO> getAllActiveCandidate();
+	
+	List<CandidateResponseDTO> getAllInActiveCandidate();
+	
+	List<CandidateResponseDTO> getCandidatesByLocation(String Location);
+	
+	List<CandidateResponseDTO> getCandidatesByGender(Gender gender);
+	
+	List<CandidateResponseDTO> getCandidatesByMaxExperience(int years);
+	
+	List<CandidateResponseDTO> getCandidatesByMinExperience(int years);
+	
+	CandidateResponseDTO updateCandidate(Long id, CandidateRequestDTO candidateRequestDTO);
+	
+	void deactivateCandidate(Long id);
+	
+	void deleteCandidate(Long id);
+	
+	
+}
