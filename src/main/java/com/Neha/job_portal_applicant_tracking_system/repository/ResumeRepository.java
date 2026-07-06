db453e969c5f8f72f33ace1e18baa412c0f9447a
Neha Sharma <neha2sharma15@gmail.com>
2026-07-06 14:00:22 +0530

    Created ResumeRepository Interface

---
+++ src/main/java/com/Neha/job_portal_applicant_tracking_system/repository/ResumeRepository.java
@@ -0,0 +1,24 @@
+package com.Neha.job_portal_applicant_tracking_system.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.Neha.job_portal_applicant_tracking_system.entity.Resume;
+
+public interface ResumeRepository {
+
+	 // Get all resumes of a candidate
+    List<Resume> findByCandidateId(Long candidateId);
+
+    // Get the default resume of a candidate
+    Optional<Resume> findByCandidateIdAndIsDefaultTrue(Long candidateId);
+
+    // Check if candidate already has a default resume
+    boolean existsByCandidateIdAndIsDefaultTrue(Long candidateId);
+
+    // Check if resume with same fileName exists for a candidate
+    boolean existsByCandidateIdAndFileName(Long candidateId, String fileName);
+
+    // Count how many resumes a candidate has
+    int countByCandidateId(Long candidateId);
+}
