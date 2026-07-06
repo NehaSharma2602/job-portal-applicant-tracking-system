0904e651657f497079447bb4feac83034a49bd4b
Neha Sharma <neha2sharma15@gmail.com>
2026-07-06 13:52:47 +0530

    Created ResumeNotFoundException

---
+++ src/main/java/com/Neha/job_portal_applicant_tracking_system/exception/ResumeNotFoundException.java
@@ -0,0 +1,9 @@
+package com.Neha.job_portal_applicant_tracking_system.exception;
+
+public class ResumeNotFoundException extends RuntimeException {
+	public ResumeNotFoundException(String message) {
+		// TODO Auto-generated constructor stub
+		super(message);
+	}
+
+}
