1d66e44fe415cc85cc9c5385260467c4b3763d63
Neha Sharma <neha2sharma15@gmail.com>
2026-07-06 14:07:08 +0530

    Created ResumeServiceInterface

---
+++ src/main/java/com/Neha/job_portal_applicant_tracking_system/service/ResumeService.java
@@ -0,0 +1,21 @@
+package com.Neha.job_portal_applicant_tracking_system.service;
+
+import java.util.List;
+
+import com.Neha.job_portal_applicant_tracking_system.dto.ResumeRequestDTO;
+import com.Neha.job_portal_applicant_tracking_system.dto.ResumeResponseDTO;
+
+public interface ResumeService {
+
+	ResumeResponseDTO uploadResume(ResumeRequestDTO resumeRequestDTO);
+	
+	ResumeResponseDTO getResumeById(Long id);
+	
+	List<ResumeResponseDTO> getResumesByCandidateId(Long candidateId);
+	
+	ResumeResponseDTO getDefaultResume(Long candidateId);
+	
+	ResumeResponseDTO setDefaultResume(Long resumeId, long candidateId);
+	
+	void deleteResume(Long id);
+}
