fc52f85067d8bef0d3ea34450821cbbc9bcae4cf
Neha Sharma <neha2sharma15@gmail.com>
2026-07-06 13:51:55 +0530

    Created DefaultResumeAlreadyExistsException

---
+++ src/main/java/com/Neha/job_portal_applicant_tracking_system/exception/DefaultResumeAlreadyExistsException.java
@@ -0,0 +1,8 @@
+package com.Neha.job_portal_applicant_tracking_system.exception;
+
+public class DefaultResumeAlreadyExistsException extends RuntimeException {
+
+	public DefaultResumeAlreadyExistsException(String message) {
+		super(message);
+	}
+}
