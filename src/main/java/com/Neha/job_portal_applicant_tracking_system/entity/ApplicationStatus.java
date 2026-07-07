4005e5c8b6c69f8310588d8774a5872668e0031a
Neha Sharma <neha2sharma15@gmail.com>
2026-07-07 14:32:19 +0530

    Created enum class for Application status

---
+++ src/main/java/com/Neha/job_portal_applicant_tracking_system/entity/ApplicationStatus.java
@@ -0,0 +1,10 @@
+package com.Neha.job_portal_applicant_tracking_system.entity;
+
+public enum ApplicationStatus {
+
+	APPLIED,        // Candidate has just applied
+    UNDER_REVIEW,   // Recruiter is reviewing the application
+    SHORTLISTED,    // Candidate has been shortlisted
+    REJECTED,       // Application has been rejected
+    SELECTED 
+}
