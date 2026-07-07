9edb49494589fa707ef3835049112486c810f408
Neha Sharma <neha2sharma15@gmail.com>
2026-07-07 14:38:55 +0530

    Created DuplicateApplicationException

---
+++ src/main/java/com/Neha/job_portal_applicant_tracking_system/exception/DuplicateApplicationException.java
@@ -0,0 +1,7 @@
+package com.Neha.job_portal_applicant_tracking_system.exception;
+
+public class DuplicateApplicationException extends RuntimeException {
+    public DuplicateApplicationException(String message) {
+        super(message);
+    }
+}
