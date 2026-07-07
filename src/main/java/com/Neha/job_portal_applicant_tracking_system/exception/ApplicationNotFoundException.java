def9c43432005a2fa467c021692bbc5ae6a0874d
Neha Sharma <neha2sharma15@gmail.com>
2026-07-07 14:37:49 +0530

    Created ApplicationNotFoundException

---
+++ src/main/java/com/Neha/job_portal_applicant_tracking_system/exception/ApplicationNotFoundException.java
@@ -0,0 +1,7 @@
+package com.Neha.job_portal_applicant_tracking_system.exception;
+
+public class ApplicationNotFoundException extends RuntimeException {
+    public ApplicationNotFoundException(String message) {
+        super(message);
+    }
+}
