dd2e514e67ed864899d29f72bd60292b5874f833
Neha Sharma <neha2sharma15@gmail.com>
2026-07-11 11:16:14 +0530

    Created InterviewNotFoundException

---
+++ src/main/java/com/Neha/job_portal_applicant_tracking_system/exception/InterviewNotFoundException.java
@@ -0,0 +1,7 @@
+package com.Neha.job_portal_applicant_tracking_system.exception;
+
+public class InterviewNotFoundException extends RuntimeException {
+    public InterviewNotFoundException(String message) {
+        super(message);
+    }
+}
