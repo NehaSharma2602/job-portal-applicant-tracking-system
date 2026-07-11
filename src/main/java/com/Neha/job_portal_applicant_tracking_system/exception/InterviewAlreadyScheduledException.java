2bcf778767741faf924ecc93f1ae7fb7e49c2234
Neha Sharma <neha2sharma15@gmail.com>
2026-07-11 11:16:54 +0530

    Created InterviewAlreadyScheduledException

---
+++ src/main/java/com/Neha/job_portal_applicant_tracking_system/exception/InterviewAlreadyScheduledException.java
@@ -0,0 +1,7 @@
+package com.Neha.job_portal_applicant_tracking_system.exception;
+
+public class InterviewAlreadyScheduledException extends RuntimeException {
+    public InterviewAlreadyScheduledException(String message) {
+        super(message);
+    }
+}
