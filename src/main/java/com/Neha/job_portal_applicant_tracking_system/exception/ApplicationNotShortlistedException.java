f99fab643cf11453c793617d1954257f3a612194
Neha Sharma <neha2sharma15@gmail.com>
2026-07-11 11:17:44 +0530

    Created ApplicationNotShortlistedException

---
+++ src/main/java/com/Neha/job_portal_applicant_tracking_system/exception/ApplicationNotShortlistedException.java
@@ -0,0 +1,7 @@
+package com.Neha.job_portal_applicant_tracking_system.exception;
+
+public class ApplicationNotShortlistedException extends RuntimeException {
+    public ApplicationNotShortlistedException(String message) {
+        super(message);
+    }
+}
