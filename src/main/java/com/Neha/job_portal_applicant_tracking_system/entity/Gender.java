bd6bdc30fe54feb0f6c2519110ebcc973ff26de0
Neha Sharma <neha2sharma15@gmail.com>
2026-07-23 12:56:27 +0530

    Updated Gender Enum with javadoc

--- src/main/java/com/Neha/job_portal_applicant_tracking_system/entity/Gender.java
+++ src/main/java/com/Neha/job_portal_applicant_tracking_system/entity/Gender.java
@@ -1,8 +1,17 @@
 package com.Neha.job_portal_applicant_tracking_system.entity;
 
-
+/**
+ * Enum representing the gender of a Candidate.
+ * Stored as a String in the database using EnumType.STRING for human-readable values.
+ */
 public enum Gender {
+	
+	 /** Male gender */
 	MALE,
+	
+	/** Female gender */
 	FEMALE,
+	
+	/** Other or prefer not to say */
 	OTHER
 }
